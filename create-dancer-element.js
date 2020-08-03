ea01e7ea2f0e2bdc17575a66c8db43db89991087
Chris Black <black@chrisblack.net>
2020-08-03 18:50:47 -0400

    Organised a bit.

--- create-dancer-element.js
+++ create-dancer-element.js
@@ -1,9 +1,18 @@
 import { emojiArray } from "./emoji-array.js";
 
+/**
+ * Selects random emoji from emoji array
+ */
 function getRandomEmoji() {
     return emojiArray[Math.floor(Math.random() * emojiArray.length)];
 }
 
+/**
+ * Creates a DOM element for a dancer
+ * @param danceFloor The dence floor DOM element
+ * @param index The index of the current dancer
+ * @param numberOfDancers The total number of dancers
+ */
 export function createDancerElement(danceFloor, index, numberOfDancers) {
     const newDancer = document.createElement("span");
     newDancer.id = index;
--- index.js
+++ index.js
@@ -1,36 +1,42 @@
 import { Dancer } from "./dancer.js";
 import { createDancerElement } from "./create-dancer-element.js";
+import { createAnalyserNode } from "./create-analyser-node.js";
 
 const danceFloor = document.getElementById("dancefloor");
 
-function nearestPowerOf2(number) {
-    return Math.pow(2, Math.round(Math.log(number) / Math.log(2)));
-}
+// Number of dancers must be a power of 2 for the AnalyserNode fftSize property
+// The lower the divisor of danceFloor.offsetWidth, the higher the number of dancers
+const numberOfDancers = Math.pow(
+    2,
+    Math.round(Math.log(danceFloor.offsetWidth / 30) / Math.log(2))
+);
 
-// Set number of dancers.
-const numberOfDancers = nearestPowerOf2(dancefloor.offsetWidth / 30);
+// Array to hold the Dancer class instances
+const dancers = [];
 
-let dancers = [];
-
+// Quietest dB level to use
 const volumeSensitivity = -120;
 
+// Create the dancer elements
 populateDanceFloor(numberOfDancers);
 
+/**
+ * Creates dancer elements in the DOM and adds instatiated Dancers to the dancers array
+ * @param numberOfDancers The number of dancers to create
+ */
 function populateDanceFloor(numberOfDancers) {
     for (let i = 0; i < numberOfDancers; i++) {
+        // Get the DOM element
         const newDancer = createDancerElement(danceFloor, i, numberOfDancers);
+        // Append it
         danceFloor.appendChild(newDancer);
+        // Instatiate a Dance class and push it to the dancers array
         dancers.push(new Dancer(newDancer, i, numberOfDancers, danceFloor));
     }
 }
 
-window.AudioContext =
-    window.AudioContext || window.webkitAudioContext || window.mozAudioContext;
-
-const audioCtx = new (AudioContext || webkitAudioContext)();
-
+// Listen for audio file selection
 const fileSelector = document.getElementById("audio-file");
-
 fileSelector.addEventListener("change", (fileInput) => {
     // check for file
     if (fileInput.target.files[0] == undefined) {
@@ -38,44 +44,42 @@ fileSelector.addEventListener("change", (fileInput) => {
     }
     const fileReader = new FileReader();
     fileReader.readAsArrayBuffer(fileInput.target.files[0]);
-    fileReader.onload = (e) => analyzeAudio(e);
+    fileReader.onload = (e) => dropTheNeedle(e);
 });
 
-function analyzeAudio(e) {
-    // Decode audio
-    audioCtx.decodeAudioData(e.target.result).then(function (buffer) {
-        var soundSource = audioCtx.createBufferSource();
-        soundSource.buffer = buffer;
-
-        //Create analyser node
-        const analyserNode = audioCtx.createAnalyser();
-        analyserNode.fftSize = numberOfDancers * 2;
-        analyserNode.minDecibels = volumeSensitivity;
-        analyserNode.maxDecibels = 0;
-        const bufferLength = analyserNode.frequencyBinCount;
-        const dataArray = new Uint8Array(bufferLength);
-
-        //Set up audio node network
-        soundSource.connect(analyserNode);
-        analyserNode.connect(audioCtx.destination);
-        soundSource.start(0);
-
-        dance(analyserNode, dataArray);
-    });
+/**
+ * Starts the audio analaysis process on selected file, plays, the music, and starts the animation
+ * @param e The fileSelector input changed event
+ */
+async function dropTheNeedle(e) {
+    const { analyserNode, dataArray, soundSource } = await createAnalyserNode(
+        e,
+        numberOfDancers,
+        volumeSensitivity
+    );
+    // Play the music
+    soundSource.start();
+    // Start animations
+    rockTheHouse(analyserNode, dataArray);
 }
 
-function dance(analyserNode, dataArray) {
-    //Schedule next redraw
-    requestAnimationFrame(() => dance(analyserNode, dataArray));
+/**
+ * Animates elements using decibel values of the frequency range of the music
+ * @param analyserNode The audioCtx analyser node
+ * @param dataArray The array of frequency ranges to analyse
+ */
+function rockTheHouse(analyserNode, dataArray) {
+    // Schedule next redraw
+    requestAnimationFrame(() => rockTheHouse(analyserNode, dataArray));
 
-    //Get spectrum data
+    // Decibel value of each frequency in the dataArray updated
     analyserNode.getByteFrequencyData(dataArray);
 
+    // Flash the red/black background
     document.body.style.backgroundColor = `rgb(${dataArray[1] - 100}, 0, 0)`;
 
+    // Animate the dancers
     for (let i = 0; i < dataArray.length; i++) {
         dancers[i].dance(dataArray[i]);
-        // const dancer = document.getElementById(i);
-        // dancer.style.bottom = dataArray[i] + "px";
     }
 }
