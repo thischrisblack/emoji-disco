0fa2db82e291725e9392e9a6924a497a1062a01b
Chris Black <black@chrisblack.net>
2020-08-04 14:58:27 -0400

    Deleted docs. Styled credit.

--- docs/create-overhead-light.js
+++
@@ -1,9 +0,0 @@
-export function createOverheadLight(index, numberOfLights) {
-    const lightSpacing = document.body.offsetWidth / numberOfLights;
-    const colors = ["red", "green", "blue", "yellow"];
-    const light = document.createElement("div");
-    const color = colors[index % colors.length];
-    light.classList.add("overhead-light", `light-${color}`);
-    light.style.left = index * lightSpacing + "px";
-    return light;
-}
